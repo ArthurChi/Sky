489fb496c893ec9f541a11b0749ec32bc5d2cb8d
VassilyChi <cjfire@sina.cn>
2018-07-03 20:54:19 +0800

    创建上半部分UI连接

---
+++ Sky/ViewControllers/WeatherViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Sky
+//
+//  Created by Mars on 28/09/2017.
+//  Copyright © 2017 Mars. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var weatherContainerView: UIView!
+    @IBOutlet weak var loadingFailedLabel: UILabel!
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+    }
+    
+    private func setupView() {
+        
+        
+    }
+}
+
